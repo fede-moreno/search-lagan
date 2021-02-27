db11b9641854ad06b28462976449aba8684e0254
Federico Moreno <fedemoreno14@gmail.com>
2021-02-27 04:00:28 -0300

    basic structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {AppRoutes} from './enums/app-routes.enum';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: AppRoutes.HOME, component: HomeComponent
+  },
+  {
+    path: AppRoutes.SETTINGS,
+    loadChildren: () => import('./settings/settings.module').then((m) => m.SettingsModule)
+  },
+  {
+    path: '**',
+    redirectTo: `/${AppRoutes.HOME}`,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
---
+++ src/app/enums/app-routes.enum.ts
@@ -0,0 +1,5 @@
+export enum AppRoutes {
+  HOME = '',
+  SETTINGS = 'settings',
+  DETAILS = 'details'
+}
