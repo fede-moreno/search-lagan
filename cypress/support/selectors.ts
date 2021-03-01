f7349ad4ff388902663e56c3773ea7099bb36200
Federico Moreno <fedemoreno14@gmail.com>
2021-03-01 20:55:09 -0300

    added e2e for basic flow

---
+++ cypress/support/selectors.ts
@@ -0,0 +1,8 @@
+export const selectors: {[key: string]: string} = {
+  searchBoxInput: '.searchBox input',
+  resultContainer: '.resultsContainer',
+  resultRow: '.resultsContainer-row',
+  resultRowLabel: '.resultsContainer-row label',
+  detailsContentTitle: '.Details-content h1',
+  detailsContentSubtitle: '.Details-content h2'
+};
